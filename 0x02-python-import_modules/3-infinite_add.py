e6070dd2066a7e1be56936ecae4dda45ea5c5aac
esulaimanleonard <111577374+esulaimanleonard@users.noreply.github.com>
2022-12-01 11:34:32 +0000

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys
-argv = sys.argv[1:]
-argv_count = len(argv)
-index = 1
-res = 0
-while index <= argv_count:
-res += int(sys.argv[index])
-index += 1
-print("{:d}".format(res))
+    """Print the addition of all arguments"""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
