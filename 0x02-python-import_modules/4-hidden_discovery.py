d880dd9e10c088b4b7088d5e76863931f7f1cb5c
esulaimanleonard <111577374+esulaimanleonard@users.noreply.github.com>
2022-12-01 21:06:19 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
 import sys
-import hidden_4
-for n in dir(hidden_4):
-if n[:2] != "__":
-print(n)
+import hidden_4 as hidden
+
+if __name__ != "__main__":
+    exit()
+
+for name in dir(hidden):
+    if name[0:2] != "__":
+        print(name)
