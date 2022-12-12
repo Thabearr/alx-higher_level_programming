750f874937bbc2abf66be899ad29051729856ef0
esulaimanleonard <111577374+esulaimanleonard@users.noreply.github.com>
2022-12-12 20:29:23 +0200

    Create 100-atoi.c

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _atoi - convert string to numbers
+ *
+ * Return: n
+ */
+int _atoi(void)
+{
+	/* int x; */
+	/* char arr[]; */
+
+	/* while (*s) */
+	/* { */
+	/* x++; */
+	/* if (*s >= 48 && *s <= '9') */
+	/* conv[i] = *s */
+	/* } */
+
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
