6f787e8c85e96844455d07cafa8131b76fce15dc
esulaimanleonard <111577374+esulaimanleonard@users.noreply.github.com>
2022-12-12 20:17:14 +0200

    Create 0-isupper.c

---
+++ 0-isupper.c
@@ -0,0 +1,21 @@
+/**
+ * _isupper - prints if the characters are lowercase or not
+ * 0 for lowercase 1 for uppercase
+ * @c: the character being tested
+ * Return: 0 for lowercase, 1 for uppercase
+ */
+int _isupper(int c)
+{
+	if ('A' <= c && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
