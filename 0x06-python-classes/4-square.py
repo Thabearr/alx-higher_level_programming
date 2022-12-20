c6d3b904377938637f29d33a7974bc1e5b905a7c
esulaimanleonard <111577374+esulaimanleonard@users.noreply.github.com>
2022-12-20 11:57:24 +0200

    Create 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        a = self.__size * self.__size
+        return a
