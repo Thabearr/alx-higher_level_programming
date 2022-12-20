cc0c170a66a315292f94f72b219e16b353dd0746
esulaimanleonard <111577374+esulaimanleonard@users.noreply.github.com>
2022-12-20 11:58:26 +0200

    Create 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        a = self.__size * self.__size
+        return a
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+            print()
